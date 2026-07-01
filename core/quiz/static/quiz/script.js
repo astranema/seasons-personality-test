b843121a738ba901ac14f9ef410b6eeee410e625
luna 3 <93050744+astranema@users.noreply.github.com>
2026-07-01 14:04:50 -0400

    Refactor code when start button is pressed into seperate functions (createNoButton and createYesButton)

--- core/quiz/static/quiz/script.js
+++ core/quiz/static/quiz/script.js
@@ -210,40 +210,50 @@ function main() {
     const qm = new Questions_manager(jsonString);
     start_button.addEventListener('click', () => {
         start_button.remove();
-        const yes_button = document.createElement('button');
-        yes_button.className = 'answer-button';
-        yes_button.textContent = 'Agree';
-        yes_button.style.fontWeight = 'bold';
-        yes_button.addEventListener('click', () => {
-            qm.modify_score(true);
-            qm.next_question();
-            question_text.textContent = qm.get_current_question().text;
-            if (qm.questions_answered >= 40) {
-                alert(qm.scores[0]);
-                alert(qm.scores[1]);
-                alert(qm.scores[2]);
-                alert(qm.scores[3]);
-            }
-        });
-        const no_button = document.createElement('button');
-        no_button.className = 'answer-button';
-        no_button.textContent = 'Disagree';
-        no_button.style.fontWeight = 'bold';
-        no_button.addEventListener('click', () => {
-            qm.modify_score(false);
-            qm.next_question();
-            question_text.textContent = qm.get_current_question().text;
-            if (qm.questions_answered >= 40) {
-                alert(qm.scores[0]);
-                alert(qm.scores[1]);
-                alert(qm.scores[2]);
-                alert(qm.scores[3]);
-            }
-        });
-        button_container.appendChild(yes_button);
-        button_container.appendChild(no_button);
+        createNoButton(qm, button_container, question_text);
+        createYesButton(qm, button_container, question_text);
+        // modifies textContent to the first question
         question_text.textContent = qm.get_current_question().text
     });
 }
+
+function createYesButton(qm, button_container, question_text) {
+    const yes_button = document.createElement('button');
+    yes_button.className = 'answer-button';
+    yes_button.textContent = 'Agree';
+    yes_button.style.fontWeight = 'bold';
+    yes_button.addEventListener('click', () => {
+        qm.modify_score(true);
+        qm.next_question();
+        question_text.textContent = qm.get_current_question().text;
+        if (qm.questions_answered >= 40) {
+            alert(qm.scores[0]);
+            alert(qm.scores[1]);
+            alert(qm.scores[2]);
+            alert(qm.scores[3]);
+        }
+    });
+    button_container.appendChild(yes_button);
+}
+
+function createNoButton(qm, button_container, question_text) {
+    const no_button = document.createElement('button');
+    no_button.className = 'answer-button';
+    no_button.textContent = 'Disagree';
+    no_button.style.fontWeight = 'bold';
+    no_button.addEventListener('click', () => {
+        qm.modify_score(false);
+        qm.next_question();
+        question_text.textContent = qm.get_current_question().text;
+        if (qm.questions_answered >= 40) {
+            alert(qm.scores[0]);
+            alert(qm.scores[1]);
+            alert(qm.scores[2]);
+            alert(qm.scores[3]);
+        }
+    });
+    button_container.appendChild(no_button);
+}
+
 
 main();
