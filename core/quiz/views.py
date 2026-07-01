aa58073872c05ad4c5a19c36b415ea6dfae4c27d
luna 3 <93050744+astranema@users.noreply.github.com>
2026-07-01 16:36:54 -0400

    Create view which returns JSON data for questions

--- core/quiz/views.py
+++ core/quiz/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import json
+from pathlib import Path
+from django.http import JsonResponse
 
 # Create your views here.
 def test(request):
     return render(request, 'quiz/test.html')
+
+def questionsJson(request):
+    # maps to core/quiz/questions.json
+    path = Path(Path(__file__).parent, 'questions.json')
+    jsonText = path.read_text()
+    jsonDict = json.loads(jsonText)
+    return JsonResponse(jsonDict, safe=False)
